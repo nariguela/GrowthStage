d63ef9f886e159266a23fff60927f84266b4d487
nariguela <pedrodc3048@gmail.com>
2024-09-26 19:17:30 -0300

    Configurado e incluído o gráfico para exibição dos dados da API

--- src/components/GrowthStage.tsx
+++ src/components/GrowthStage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import ReactEcharts from 'echarts-for-react'
 
 interface ApiData {
   degree_days: number;
@@ -20,10 +21,52 @@ export default function GrowthStage() {
     fetchData();
   }, [])
 
+  const getChartOptions = () => {
+    return {
+      title: {
+        text: 'Growth Stage',
+      },
+      tooltip: {
+        trigger: 'axis',
+      },
+      legend: {
+        data: ['Degree Days', 'Precipitation', 'NDVI']
+      },
+      xAxis: {
+        type: 'category',
+        data: chartData.map(data => new Date(data.time * 1000).toLocaleDateString()), 
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          name: 'Degree Days',
+          data: chartData.map(data => data.degree_days),
+          type: 'line',
+          smooth: true,
+        },
+        {
+          name: 'Precipitation',
+          data: chartData.map(data => data.precipitation),
+          type: 'line',
+          smooth: true,
+        },
+        {
+          name: 'NDVI',
+          data: chartData.map(data => data.ndvi),
+          type: 'line',
+          smooth: true,
+        },
+      ],
+    };
+  }
+  
 
   return (
     <div>
       <h2>GrowthStage</h2>
+      <ReactEcharts option={getChartOptions()} />
     </div>
   )
 }
