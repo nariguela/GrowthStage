868b5b7b22f9ebee37efb9a5565ed9b9f2f3fb69
nariguela <pedrodc3048@gmail.com>
2024-09-26 18:35:04 -0300

    Importado os dados da API

--- src/components/GrowthStage.tsx
+++ src/components/GrowthStage.tsx
@@ -1,5 +1,27 @@
+import { useEffect } from "react";
+
+
+interface ApiData {
+  degree_days: number;
+  time: number;
+  precipitation: number;
+  ndvi: number;
+}
+
 export default function GrowthStage() {
+  useEffect(function() {
+    async function fetchData() {
+      const res = await fetch('https://raw.githubusercontent.com/alexanderboliva/test/main/api_example.json');
+      const apiData: ApiData[] = await res.json();
+      console.log(apiData)
+    }
+    fetchData();
+  }, [])
+
+
   return (
-    <div>GrowthStage</div>
+    <div>
+      <h2>GrowthStage</h2>
+    </div>
   )
 }
